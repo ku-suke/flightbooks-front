4ba54fea7c124ed3cd191c00a70dac3564800093
ku-suke <gachasan@gmail.com>
2018-08-26 14:22:55 +0900

    Add Book entity

---
+++ src/entities/Book.ts
@@ -0,0 +1,23 @@
+import firebase from "firebase";
+
+export interface IBook {
+  identifier?: string;
+  title: string;
+  userId?: string;
+  genre?: number;
+  createdAt?: firebase.firestore.Timestamp;
+  updatedAt?: firebase.firestore.Timestamp;
+  thumbanilUrl?: string;
+}
+
+export default class BookEntity {
+  props: IBook
+
+  constructor(params: IBook) {
+    const genre = params.genre || 0 
+    this.props = {
+      ...params,
+      genre
+    }
+  }
+}
