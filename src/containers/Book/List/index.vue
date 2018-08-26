3dcd1f64a3296697ca0499c79fcf5ca389310402
ku-suke <gachasan@gmail.com>
2018-08-26 15:14:01 +0900

    Made a container prettier

--- src/containers/Book/List/index.vue
+++ src/containers/Book/List/index.vue
@@ -34,32 +34,36 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-import Presenter, { PresenterParams, IPresenter } from './presenter'
+import Presenter, { PresenterParams, IPresenter } from "./presenter";
 
-import { IBook } from '@/entities/Book'
-import BookEntity from '@/entities/Book'
-import BookRepository from '@/repositories/BookRepository'
-import UserRepository from '@/repositories/UserRepository'
-import ErrorService from '@/services/ErrorService'
+import { IBook } from "@/entities/Book";
+import BookEntity from "@/entities/Book";
+import BookRepository from "@/repositories/BookRepository";
+import UserRepository from "@/repositories/UserRepository";
+import ErrorService from "@/services/ErrorService";
 
 // Use Case
-import RegisterBookUseCase, { IRegisterMuseumUseCase } from '@/usecases/RegisterBookUseCase'
-import RemoveBookUseCase, { IRemoveBookUseCase } from '@/usecases/RemoveBookUseCase'
-import LoadContainerUseCase, { ILoadContainerUseCase } from './LoadContainerUseCase'
-
+import RegisterBookUseCase, {
+  IRegisterMuseumUseCase
+} from "@/usecases/RegisterBookUseCase";
+import RemoveBookUseCase, {
+  IRemoveBookUseCase
+} from "@/usecases/RemoveBookUseCase";
+import LoadContainerUseCase, {
+  ILoadContainerUseCase
+} from "./LoadContainerUseCase";
 
 // components
-import Modal from '@/components/Base/Modal.vue'
-import FormBlock from '@/components/Base/FormBlock.vue'
-import BaseInput from '@/components/Base/Input.vue'
-import Book from '@/components/Modules/Book.vue'
-
+import Modal from "@/components/Base/Modal.vue";
+import FormBlock from "@/components/Base/FormBlock.vue";
+import BaseInput from "@/components/Base/Input.vue";
+import Book from "@/components/Modules/Book.vue";
 
 interface IData {
-  project: IBook,
-  showModal: boolean
+  project: IBook;
+  showModal: boolean;
 }
 
 export default Vue.extend({
@@ -72,61 +76,68 @@ export default Vue.extend({
   data(): IData {
     return {
       project: {
-        title: '',
+        title: "",
         genre: 0
       },
       showModal: false
-    }
+    };
   },
   computed: {
     verified(): boolean {
-      return this.project.title.length > 0
+      return this.project.title.length > 0;
     },
     presenter(): IPresenter {
       const params: PresenterParams = {
-        userRepository: new UserRepository,
-        bookRepository: new BookRepository
-      }
+        userRepository: new UserRepository(),
+        bookRepository: new BookRepository()
+      };
 
-      return Presenter(params)
+      return Presenter(params);
     }
   },
   methods: {
     async register() {
-      const bookEntity = new BookEntity({...this.project, userId: this.presenter.userId})
+      const bookEntity = new BookEntity({
+        ...this.project,
+        userId: this.presenter.userId
+      });
       const params: IRegisterMuseumUseCase = {
-        bookRepository: new BookRepository,
-        errorService: new ErrorService({ context: 'RegisterMuseumUseCase' })
-      }
-      const usecase = new RegisterBookUseCase(params)
-      const identifier = await usecase.execute(bookEntity)
+        bookRepository: new BookRepository(),
+        errorService: new ErrorService({ context: "RegisterMuseumUseCase" })
+      };
+      const usecase = new RegisterBookUseCase(params);
+      const identifier = await usecase.execute(bookEntity);
     },
     async removeItem(identifier: string) {
-      const result1 = window.confirm('このプロジェクトを削除してもよろしいですか？\n(この操作は取り消せません)')
-      if (!result1) return
+      const result1 = window.confirm(
+        "このプロジェクトを削除してもよろしいですか？\n(この操作は取り消せません)"
+      );
+      if (!result1) return;
 
-      const result2 = window.confirm('本当にプロジェクトを削除してもよろしいですか？\n(この操作は取り消せません)')
-      if (!result2) return
+      const result2 = window.confirm(
+        "本当にプロジェクトを削除してもよろしいですか？\n(この操作は取り消せません)"
+      );
+      if (!result2) return;
 
       const params: IRemoveBookUseCase = {
-        bookRepository: new BookRepository,
-        errorService: new ErrorService({ context: 'RemoveBook usecase' })
-      }
-      const usecase = new RemoveBookUseCase(params)
-      await usecase.execute(identifier)
-      location.reload(true)
+        bookRepository: new BookRepository(),
+        errorService: new ErrorService({ context: "RemoveBook usecase" })
+      };
+      const usecase = new RemoveBookUseCase(params);
+      await usecase.execute(identifier);
+      location.reload(true);
     }
   },
   async mounted() {
     const params: ILoadContainerUseCase = {
-      bookRepository: new BookRepository,
+      bookRepository: new BookRepository(),
       userId: this.presenter.userId,
-      errorService: new ErrorService({context: 'LoadContainerUseCase'})
-    }
-    const usecase = new LoadContainerUseCase(params)
-    await usecase.execute()
+      errorService: new ErrorService({ context: "LoadContainerUseCase" })
+    };
+    const usecase = new LoadContainerUseCase(params);
+    await usecase.execute();
   }
-})
+});
 </script>
 
 <style scoped>
@@ -152,9 +163,10 @@ export default Vue.extend({
   height: 240px;
   width: 240px;
   border-radius: 8px;
-  box-shadow: 0 1px 2px 0 rgba(60,64,67,0.3), 0 1px 3px 1px rgba(60,64,67,0.15);
+  box-shadow: 0 1px 2px 0 rgba(60, 64, 67, 0.3),
+    0 1px 3px 1px rgba(60, 64, 67, 0.15);
   cursor: pointer;
-  transition: .3s;
+  transition: 0.3s;
 }
 
 .BookList__Project.empty {
@@ -172,5 +184,4 @@ export default Vue.extend({
 .BookList__Project:hover {
   background-color: #eee;
 }
-
 </style>
