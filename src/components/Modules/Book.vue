d77fd7fa2686bbff769ae57b1cd3c5b62e018c9f
ku-suke <gachasan@gmail.com>
2018-08-26 14:10:45 +0900

    Add Book Module

---
+++ src/components/Modules/Book.vue
@@ -0,0 +1,79 @@
+<template>
+  <a class="Book">
+    <div class="Book__Thumbnail">
+      <img :src="book.props.thumbanilUrl || 'https://images-na.ssl-images-amazon.com/images/I/5191wb7GxtL._SX350_BO1,204,203,200_.jpg'" alt="">
+    </div>
+    <div class="Book__Info">
+      <div class="Book__Title">{{ book.props.title }}</div>
+      <div class="Book__UpdatedAt">作成日: {{ updatedAt }}</div>
+    </div>
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import BookEntity from '@/entities/Book'
+import { formatDate } from '@/utils/formatter'
+
+export default Vue.extend({
+  props: {
+    book: {
+      type: Object as () => BookEntity,
+      required: true,
+    },
+  },
+  computed: {
+    updatedAt(): string {
+      const updatedAt = this.book.props.updatedAt
+
+      return updatedAt ? formatDate(updatedAt) : ''
+    }
+  }
+})
+</script>
+
+<style scoped>
+.Book {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.Book:hover > .Book__Thumbnail {
+  opacity: .6;
+}
+
+.Book__Thumbnail {
+  height: 160px;
+  width: 100%;
+  transition: .3s;
+}
+
+.Book__Thumbnail > img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+
+.Book__Info {
+  height: 80px;
+  width: calc(100% - 20px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start
+}
+
+.Book__Title {
+  font-style: italic;
+  margin-bottom: 8px;
+}
+
+.Book__UpdatedAt {
+  color: #ddd;
+  font-size: 12px;
+  font-weight: bold;
+}
+</style>
