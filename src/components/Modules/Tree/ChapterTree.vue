40ebe02e1ef42e4d89b334eb1aa8487b0c89838f
ku-suke <gachasan@gmail.com>
2018-09-08 17:39:40 +0900

    Add Tree modules

---
+++ src/components/Modules/Tree/ChapterTree.vue
@@ -0,0 +1,33 @@
+<template>
+  <Nav :label="data.props.title">
+    <div slot="menu">
+      <ChapterMenu />
+    </div>
+    <ChapterTree v-for="chapter in data.chapters" :data="chapter" :key="chapter.props.title" />
+    <PageTree v-for="page in data.pages" :key="page.props.title" :data="page" />
+  </Nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Nav from '@/components/Base/Nav.vue'
+import ChapterMenu from '@/components/Modules/ChapterMenu.vue'
+import PageTree from '@/components/Modules/Tree/PageTree.vue'
+
+import ChapterTreeEntity from '@/entities/ChapterTree'
+
+export default Vue.extend({
+  components: {
+    Nav,
+    ChapterMenu,
+    PageTree
+  },
+  name: 'ChapterTree',
+  props: {
+    data: {
+      type: Object as () => ChapterTreeEntity,
+      required: true
+    }
+  }
+})
+</script>
---
+++ src/components/Modules/Tree/PageTree.vue
@@ -0,0 +1,24 @@
+<template>
+  <NavItem>
+    {{ data.props.title }}
+  </NavItem>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import NavItem from '@/components/Base/NavItem.vue'
+
+import PageTreeEntity from '@/entities/PageTree'
+
+export default Vue.extend({
+  components: {
+    NavItem
+  },
+  props: {
+    data: {
+      type: Object as () => PageTreeEntity,
+      required: true
+    }
+  }
+})
+</script>
