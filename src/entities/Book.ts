c3e5fd621c48cd4cbcf5e7f35bd15289c48e5ec8
ku-suke <gachasan@gmail.com>
2018-09-22 13:03:54 +0900

    Update Book Entity

--- src/entities/Book.ts
+++ src/entities/Book.ts
@@ -9,17 +9,23 @@ export interface IBook {
   updatedAt?: firebase.firestore.Timestamp;
   thumbanilUrl?: string;
   content: string
-  projectTree?: firebase.firestore.DocumentReference
+  projectTree: firebase.firestore.DocumentReference
 }
 
 export default class BookEntity {
-  props: IBook
+  private props: IBook
 
   constructor(params: IBook) {
     const genre = params.genre || 0 
     this.props = {
       ...params,
       genre
+    }
+  }
+
+  getProps(): IBook {
+    return {
+      ...this.props
     }
   }
 }
