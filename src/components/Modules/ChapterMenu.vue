1e5fc856df487a784e860b8277f17bc7a4dba2cd
ku-suke <gachasan@gmail.com>
2018-09-22 20:45:54 +0900

    Show register chapter modal on ChapterMenu

--- src/components/Modules/ChapterMenu.vue
+++ src/components/Modules/ChapterMenu.vue
@@ -1,17 +1,52 @@
 <template>
   <div class="ChapterMenu">
-    <button class="ChapterMenu__Item" title="チャプターを追加" @click="addChapter">
+    <button class="ChapterMenu__Item" title="チャプターを追加" @click="showRegisterChapterModal = true">
       <i class="el-icon-tickets" />
     </button>
+    <Modal v-show="showRegisterChapterModal" @close="showRegisterChapterModal = false">
+      <div slot="header">
+        <h2>チャプターを追加</h2>
+      </div>
+      <template slot="body">
+        <form class="ChapterMenu__Form" @submit.prevent="addChapter">
+          <FormBlock label="チャプター名">
+            <BaseInput placeholder="My New Project" v-model="newChapterName"/>
+          </FormBlock>
+          <el-button type="primary" native-type="submit" :disabled="!newChapterName" round>追加</el-button>
+        </form>
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Modal from "@/components/Base/Modal.vue";
+import FormBlock from "@/components/Base/FormBlock.vue";
+import BaseInput from "@/components/Base/Input.vue";
+
+interface IData {
+  showRegisterChapterModal: boolean,
+  newChapterName: string
+}
+
 export default Vue.extend({
+  components: {
+    Modal,
+    FormBlock,
+    BaseInput
+  },
+  data(): IData {
+    return {
+      showRegisterChapterModal: false,
+      newChapterName: ''
+    }
+  },
   methods: {
     addChapter() {
-      this.$emit('addChapter', 'hoge')
+      this.$emit('addChapter', this.newChapterName)
+      this.newChapterName = ''
+      this.showRegisterChapterModal = false
     }
   }
 })
@@ -39,6 +74,12 @@ export default Vue.extend({
 .ChapterMenu__Item:focus {
   box-shadow: 1px 1px 1px 1px rgba(85,85,85,1);
   box-shadow: 0px 0px 26px -10px rgba(0,0,0,0.75);
+}
+
+.ChapterMenu__Form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 </style>
