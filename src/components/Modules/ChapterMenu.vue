73fa4c29f56a8d4d6ee01a89a05266b58a8f8521
ku-suke <gachasan@gmail.com>
2018-09-23 11:21:31 +0900

    Add Edit button on ChapterMenu

--- src/components/Modules/ChapterMenu.vue
+++ src/components/Modules/ChapterMenu.vue
@@ -3,6 +3,9 @@
     <button class="ChapterMenu__Item" title="チャプターを追加" @click="showRegisterChapterModal = true">
       <Icon name="add-folder" />
     </button>
+    <button class="ChapterMenu__Item" title="名前を変更" @click="openRenameModal">
+      <Icon name="edit" />
+    </button>
     <Modal v-show="showRegisterChapterModal" @close="showRegisterChapterModal = false">
       <div slot="header">
         <h2>チャプターを追加</h2>
@@ -13,6 +16,19 @@
             <BaseInput placeholder="My New Project" v-model="newChapterName"/>
           </FormBlock>
           <el-button type="primary" native-type="submit" :disabled="!newChapterName" round>追加</el-button>
+        </form>
+      </template>
+    </Modal>
+    <Modal v-show="showRenameChapterModal" @close="showRenameChapterModal = false">
+      <div slot="header">
+        <h2>名前を変更</h2>
+      </div>
+      <template slot="body">
+        <form class="ChapterMenu__Form" @submit.prevent="renameChapter">
+          <FormBlock label="チャプター名">
+            <BaseInput placeholder="My New Project" v-model="newChapterName"/>
+          </FormBlock>
+          <el-button type="primary" native-type="submit" :disabled="!newChapterName" round>更新</el-button>
         </form>
       </template>
     </Modal>
@@ -28,6 +44,7 @@ import Icon from '@/components/Base/Icon.vue'
 
 interface IData {
   showRegisterChapterModal: boolean,
+  showRenameChapterModal: boolean,
   newChapterName: string
 }
 
@@ -41,6 +58,7 @@ export default Vue.extend({
   data(): IData {
     return {
       showRegisterChapterModal: false,
+      showRenameChapterModal: false,
       newChapterName: ''
     }
   },
@@ -49,8 +67,16 @@ export default Vue.extend({
       this.$emit('addChapter', this.newChapterName)
       this.newChapterName = ''
       this.showRegisterChapterModal = false
+    },
+    openRenameModal() {
+      this.showRenameChapterModal = true
+    },
+    renameChapter() {
+      this.$emit('renameChapter', this.newChapterName)
+      this.newChapterName = ''
+      this.showRenameChapterModal = false
     }
-  }
+  },
 })
 </script>
 
