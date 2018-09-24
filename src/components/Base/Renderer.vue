094eedf463aae140b98705bb2a56524784cbc457
ku-suke <gachasan@gmail.com>
2018-09-25 04:23:12 +0900

    Add Renderer component

---
+++ src/components/Base/Renderer.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Vue, { VNode } from 'vue';
+export default Vue.extend({
+  props: {
+    vnode: {
+      type: Object as () => VNode,
+      required: true
+    }
+  },
+  render() {
+    return this.vnode;
+  }
+});
+</script>
