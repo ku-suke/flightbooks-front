1421270a2276005d69f7b1cf849079636624d346
ku-suke <gachasan@gmail.com>
2018-09-27 01:26:10 +0900

    Add NewBookEntity method

--- src/entities/Book.ts
+++ src/entities/Book.ts
@@ -1,31 +1,48 @@
 import firebase from "firebase";
+import uuid from "uuid/v4";
 
 export interface IBook {
-  identifier?: string;
-  title: string;
-  userId?: string;
+  identifier: string;
+  name: string;
+  owner?: string;
   genre?: number;
-  createdAt?: firebase.firestore.Timestamp;
-  updatedAt?: firebase.firestore.Timestamp;
+  createdAt: firebase.firestore.Timestamp;
+  updatedAt: firebase.firestore.Timestamp;
   thumbanilUrl?: string;
-  content: string;
-  projectTree: firebase.firestore.DocumentReference;
+  projectTreeRef: firebase.firestore.DocumentReference;
 }
 
 export default class BookEntity {
-  private props: IBook;
+  private _props: IBook;
 
   constructor(params: IBook) {
     const genre = params.genre || 0;
-    this.props = {
+    this._props = {
       ...params,
       genre
     };
   }
 
-  getProps(): IBook {
-    return {
-      ...this.props
-    };
+  static newEntity({
+    name,
+    owner,
+    projectTreeRef
+  }: {
+    name: string;
+    owner: string;
+    projectTreeRef: firebase.firestore.DocumentReference;
+  }): BookEntity {
+    return new BookEntity({
+      identifier: uuid(),
+      name,
+      owner,
+      projectTreeRef,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp() as firebase.firestore.Timestamp,
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp() as firebase.firestore.Timestamp
+    });
+  }
+
+  get props(): IBook {
+    return this._props;
   }
 }
