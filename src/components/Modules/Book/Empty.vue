3e164d290ff998f4d77b995a80cb0d99c0fde75c
ku-suke <gachasan@gmail.com>
2018-09-26 23:50:51 +0900

    Add BookEmpty component

---
+++ src/components/Modules/Book/Empty.vue
@@ -0,0 +1,43 @@
+<template>
+  <button class="BookEmpty" @click="handleClick">
+    <div class="BookEmpty__Icon">
+      <i class="el-icon-plus" />
+    </div>
+    <div class="BookEmpty__Text">新規プロジェクトを作成</div>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  methods: {
+    handleClick() {
+      this.$emit("click");
+    }
+  }
+});
+</script>
+
+<style scoped>
+.BookEmpty {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 240px;
+  width: 240px;
+  border-radius: var(--radius-lv2);
+  box-shadow: var(--shadow-normal);
+  cursor: pointer;
+  transition: var(--transition-normal);
+  font-size: var(--fontSize-normal);
+}
+
+.BookEmpty:hover {
+  background-color: var(--color-light-gray);
+}
+
+.BookEmpty__Icon {
+  margin-bottom: var(--space-lv1);
+}
+</style>
