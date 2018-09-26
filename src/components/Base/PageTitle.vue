eb403f64aab352a0e89c677798b91b89e411ed81
ku-suke <gachasan@gmail.com>
2018-09-26 23:51:49 +0900

    Add PageTitle component

---
+++ src/components/Base/PageTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="PageTitle">{{ label }}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
+
+<style scoped>
+.PageTitle {
+  font-size: var(--fontSize-h1);
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  margin-bottom: var(--space-lv4);
+}
+</style>
