927465b38c0c8b4eade5a34cf447c5614c660b19
ku-suke <gachasan@gmail.com>
2018-09-27 01:28:41 +0900

    Update RegisterBook UseCase

--- src/usecases/RegisterBookUseCase.ts
+++ src/usecases/RegisterBookUseCase.ts
@@ -1,26 +1,47 @@
-import BaseUseCase from "@/usecases/BaseUseCase";
 import BookRepository from "@/repositories/BookRepository";
+import ProjectTreeRepository from "@/repositories/ProjectTreeRepository";
 import BookEntity from "@/entities/Book";
+import ProjectTreeEntity from "@/entities/ProjectTree";
 import ErrorService from "@/services/ErrorService";
 
-export interface IRegisterMuseumUseCase {
+export interface IRegisterBookUseCase {
   bookRepository: BookRepository;
+  projectTreeRepository: ProjectTreeRepository;
   errorService: ErrorService;
 }
 
-export default class RegisterMuseumUseCase implements BaseUseCase {
+export default class RegisterBookUseCase implements UseCase {
   bookRepository: BookRepository;
+  projectTreeRepository: ProjectTreeRepository;
   errorService: ErrorService;
 
-  constructor({ bookRepository, errorService }: IRegisterMuseumUseCase) {
+  constructor({
+    bookRepository,
+    projectTreeRepository,
+    errorService
+  }: IRegisterBookUseCase) {
     this.bookRepository = bookRepository;
+    this.projectTreeRepository = projectTreeRepository;
     this.errorService = errorService;
   }
 
-  async execute(entity: BookEntity): Promise<string> {
+  async execute({
+    name,
+    owner
+  }: {
+    name: string;
+    owner: string;
+  }): Promise<void> {
     try {
-      const identifier = await this.bookRepository.registerItem(entity);
-      return identifier;
+      // Register Empty ProjectTree
+      const projectTreeEntity = ProjectTreeEntity.newEntity({ owner });
+      const projectTreeRef = await this.projectTreeRepository.create(
+        projectTreeEntity
+      );
+
+      // Register New Book
+      const bookEntity = BookEntity.newEntity({ name, owner, projectTreeRef });
+      await this.bookRepository.create(bookEntity);
     } catch (error) {
       await this.errorService.handle(error);
       throw new Error(error);
