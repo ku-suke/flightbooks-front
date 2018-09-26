f183fae628b1bbc9f7126599ba0887628f3ff73d
ku-suke <gachasan@gmail.com>
2018-09-26 23:51:24 +0900

    Add BookCard component

--- src/components/Modules/Book/Card.vue
+++ src/components/Modules/Book/Card.vue
@@ -1,14 +1,14 @@
 <template>
   <transition name="book">
-    <router-link class="Book" :to="{name: 'project', params: {id: book.props.identifier}}">
-      <div class="Book__Thumbnail">
+    <router-link class="BookCard" :to="{name: 'project', params: {id: book.props.identifier}}">
+      <div class="BookCard__Thumbnail">
         <img :src="book.props.thumbanilUrl || 'https://images-na.ssl-images-amazon.com/images/I/5191wb7GxtL._SX350_BO1,204,203,200_.jpg'" alt="">
       </div>
-      <div class="Book__Info">
-        <div class="Book__Title">{{ book.props.title }}</div>
-        <div class="Book__UpdatedAt">作成日: {{ updatedAt }}</div>
+      <div class="BookCard__Info">
+        <div class="BookCard__Title">{{ book.props.title }}</div>
+        <div class="BookCard__UpdatedAt">作成日: {{ updatedAt }}</div>
       </div>
-      <div class="Book__Remove" @click="handleRemove">
+      <div class="BookCard__Remove" @click="handleRemove">
         <el-button type="danger" icon="el-icon-close" circle />
       </div>
     </router-link>
@@ -43,63 +43,74 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-.Book {
-  height: 100%;
-  width: 100%;
+.BookCard {
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 240px;
+  width: 240px;
+  border-radius: var(--radius-lv2);
+  box-shadow: var(--shadow-normal);
   position: relative;
   text-decoration: none;
-  color: #070707;
+  color: var(--color-black);
+  transition: var(--transition-normal);
 }
 
-.Book:hover > .Book__Thumbnail {
+.BookCard:hover {
+  background-color: var(--color-light-gray);
+}
+
+.BookCard:hover > .BookCard__Thumbnail {
   opacity: 0.6;
 }
 
-.Book__Thumbnail {
+.BookCard__Thumbnail {
   height: 160px;
   width: 100%;
-  transition: 0.3s;
+  transition: var(--transition-normal);
 }
 
-.Book__Thumbnail > img {
+.BookCard__Thumbnail > img {
   height: 100%;
   width: 100%;
   object-fit: cover;
 }
 
-.Book__Info {
-  height: 80px;
-  width: calc(100% - 20px);
+.BookCard__Info {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
+  height: 80px;
+  width: 100%;
+  padding: var(--space-lv2);
+  box-sizing: border-box;
 }
 
-.Book__Title {
+.BookCard__Title {
   font-style: italic;
-  margin-bottom: 8px;
+  margin-bottom: var(--space-lv1);
+  font-size: var(--fontSize-normal);
 }
 
-.Book__UpdatedAt {
-  color: #ddd;
-  font-size: 12px;
-  font-weight: bold;
+.BookCard__UpdatedAt {
+  color: var(--color-gray);
+  font-size: var(--fontSize-subText);
+  font-weight: var(--fontWeight-bold);
 }
 
-.Book__Remove {
+.BookCard__Remove {
   position: absolute;
   top: 0;
   right: 0;
   transform: translate(50%, -50%);
   display: none;
-  transition: 0.5s;
+  transition: var(--transition-slow);
 }
 
-.Book:hover > .Book__Remove {
+.BookCard:hover > .BookCard__Remove,
+.BookCard:focus > .BookCard__Remove {
   display: block;
 }
 
