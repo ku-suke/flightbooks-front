8f479c7933a24eca980620e750c994f011176872
ku-suke <gachasan@gmail.com>
2018-09-27 21:57:04 +0900

    Update book nav presenter

--- src/containers/Book/Detail/Nav/presenter.ts
+++ src/containers/Book/Detail/Nav/presenter.ts
@@ -1,23 +1,29 @@
+import UserRepository from "@/repositories/UserRepository";
 import ProjectTreeRepository from "@/repositories/ProjectTreeRepository";
 import PageContentRepository from "@/repositories/PageContentRepository";
 import ProjectTreeEntity from "@/entities/ProjectTree";
 
 export interface PresenterParams {
+  userRepository: UserRepository;
   projectTreeRepository: ProjectTreeRepository;
   pageContentRepository: PageContentRepository;
 }
 
 export interface IPresenter {
+  userId: string;
   projectTree: ProjectTreeEntity;
   currentPage: string | null;
 }
 
 export default ({
+  userRepository,
   projectTreeRepository,
   pageContentRepository
 }: PresenterParams): IPresenter => {
+  const currentUser = userRepository.getCurrentUser();
   const pageContent = pageContentRepository.getItem();
   return {
+    userId: currentUser ? currentUser.uid : "",
     projectTree: projectTreeRepository.getData(),
     currentPage: pageContent ? pageContent.props.identifier : null
   };
