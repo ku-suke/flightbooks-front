feb600481cff1326ba52a57f0d67a2212b02919f
ku-suke <gachasan@gmail.com>
2018-09-27 21:57:33 +0900

    Add BuildJob Entity factory

--- src/entities/BuildJob.ts
+++ src/entities/BuildJob.ts
@@ -1,14 +1,37 @@
+import firebase from "firebase";
+import uuid from "uuid/v4";
+
 export enum JobStatus {
   JOB_CREATED = "job created",
   JOB_ERROR = "job error"
 }
 
 export interface IBuildJob {
+  identifier: string;
   bookId: string;
-  key: string;
+  key?: string;
   owner: string;
   status: JobStatus;
+  pageIds: firebase.firestore.DocumentReference[];
 }
+
+export const buildJobEntityFactory = ({
+  bookId,
+  owner,
+  pageIds
+}: {
+  bookId: string;
+  owner: string;
+  pageIds: firebase.firestore.DocumentReference[];
+}) => {
+  return new BuildJobEntity({
+    identifier: uuid(),
+    bookId,
+    owner,
+    status: JobStatus.JOB_CREATED,
+    pageIds
+  });
+};
 
 export default class BuildJobEntity {
   private _props: IBuildJob;
