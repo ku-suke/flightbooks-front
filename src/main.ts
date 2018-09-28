7c092264ff58ff9e10de49b39d440fdef2c184d4
ku-suke <gachasan@gmail.com>
2018-09-28 14:44:20 +0900

    Remove Service Worker

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import "@/assets/css/element/index.css";
 import App from "./App.vue";
 import router from "@/router";
 import store from "@/store";
-import "@/registerServiceWorker";
+// import "@/registerServiceWorker";
 import initFirebase from "@/middleware/firebase_middleware";
 
 Vue.config.productionTip = false;
